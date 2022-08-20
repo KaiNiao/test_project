f0d56ed9d27d8029cd572e4f9edf7625bd328d34
KaiNiao <49524737+KaiNiao@users.noreply.github.com>
2022-08-20 22:35:55 +0800

    [Bytebase] Create latest schema for "db1" after migration 202208201230
    THIS COMMIT IS AUTO-GENERATED BY BYTEBASE
    
    http://localhost:8080/issue/add-column-for-table-t-by-prod-db1__202208201230__migrate__add_column_for_table_t-sql-102?stage=103
    
    --------Original migration change--------
    
    https://github.com/KaiNiao/test_project/commit/1201d42f86ea283d68a2da96c709911ae4b706ce
    
    retry mysql ddl file

---
+++ bytebase/prod/.db1__LATEST.sql
@@ -0,0 +1,10 @@
+--
+-- Table structure for `t`
+--
+CREATE TABLE `t` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT '' COMMENT 'name',
+  `a` varchar(10) DEFAULT '' COMMENT 'a',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
